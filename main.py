906d644e5bced1fa5688ecebbd3f37525ff598f7
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-03-05 16:46:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 from random import randint
 pc_choice = randint(1,50)
+arr = [1,2,3,4,5,6]
 
-user_P = 100
-play = True
-while play:
-     user_c = int(input(f"chose number(1-50){user_P}:"))
-     if user_P <= 0:
-          print(f"FUCK LUSER{user_P}")
-          play = False
-     elif user_c > pc_choice:
-          user_P = user_P - 20
-          print(f"lower{user_P}")
-     elif user_c < pc_choice:
-          user_P = user_P - 20
-          print(f"HIGHER{user_P}")
-     elif user_c == pc_choice:
-          user_P = user_P + 20
-          print(f"GET Money!!,{pc_choice},user_p = {user_P} ")
-          pc_choice2 = randint(1, 50)
-          while play:
-               user_c = int((input(f"chose number(1-50){user_P}:")))
-               if user_P <= 0:
-                    print(f"FUCK LUSER{user_P}")
-                    play = False
-               elif user_c > pc_choice2:
-                    user_P = user_P - 20
-                    print(f"lower{user_P}")
-               elif user_c < pc_choice2:
-                    user_P = user_P - 20
-                    print(f"HIGHER{user_P}")
-               elif user_c == pc_choice2:
-                    user_P = user_P + 20
-                    print(f"GET Money!!,{pc_choice2},user_p = {user_P}  ")
-                    
+def casino():
+     user_P = 100
+     play = True
+     while play:
+          user_c = int(input(f"chose number(1-50){user_P}:"))
+          if user_P <= 0:
+               print(f"FUCK LUSER{user_P}")
+               play = False
+          elif user_c > pc_choice:
+               user_P = user_P - 20
+               print(f"lower{user_P}")
+          elif user_c < pc_choice:
+               user_P = user_P - 20
+               print(f"HIGHER{user_P}")
+          elif user_c == pc_choice:
+               user_P = user_P + 20
+               print(f"GET Money!!,{pc_choice},user_p = {user_P} ")
+               pc_choice2 = randint(1, 50)
+               while play:
+                    user_c = int((input(f"chose number(1-50){user_P}:")))
+                    if user_P <= 0:
+                         print(f"FUCK LUSER{user_P}")
+                         play = False
+                    elif user_c > pc_choice2:
+                         user_P = user_P - 20
+                         print(f"lower{user_P}")
+                    elif user_c < pc_choice2:
+                         user_P = user_P - 20
+                         print(f"HIGHER{user_P}")
+                    elif user_c == pc_choice2:
+                         user_P = user_P + 20
+                         print(f"GET Money!!,{pc_choice2},user_p = {user_P}  ")
+
+for ar in arr:
+     casino()    
+
      
      
 
