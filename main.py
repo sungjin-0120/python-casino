425c8ea5546c0716fc75a960ec1e2b9774576ff0
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-03-05 17:18:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 from random import randint
-pc_choice = randint(1,50)
+
 arr = [1,2,3,4,5,6]
 
 def casino():
-     user_P = 100
+     user_P = 500
      play = True
+     pc_choice = randint(1,50)
      while play:
+              
           user_c = int(input(f"chose number(1-50){user_P}:"))
           if user_P <= 0:
-               print(f"FUCK LUSER{user_P}")
+               print(f"FUCK LUSER{user_P},{pc_choice}")
                play = False
           elif user_c > pc_choice:
                user_P = user_P - 20
-               print(f"lower{user_P}")
+               print(f"lower{user_P},{pc_choice}")
           elif user_c < pc_choice:
                user_P = user_P - 20
-               print(f"HIGHER{user_P}")
+               print(f"HIGHER{user_P},{pc_choice}")
           elif user_c == pc_choice:
                user_P = user_P + 20
                print(f"GET Money!!,{pc_choice},user_p = {user_P} ")
-               pc_choice2 = randint(1, 50)
-               while play:
-                    user_c = int((input(f"chose number(1-50){user_P}:")))
-                    if user_P <= 0:
-                         print(f"FUCK LUSER{user_P}")
-                         play = False
-                    elif user_c > pc_choice2:
-                         user_P = user_P - 20
-                         print(f"lower{user_P}")
-                    elif user_c < pc_choice2:
-                         user_P = user_P - 20
-                         print(f"HIGHER{user_P}")
-                    elif user_c == pc_choice2:
-                         user_P = user_P + 20
-                         print(f"GET Money!!,{pc_choice2},user_p = {user_P}  ")
+               pc_choice = randint(1,50)
+               #pc_choice가 같은 번호가 되어지지 않게 끝나고 나서 한번더 선언
 
-for ar in arr:
-     casino()    
+
+casino()    
 
      
      
